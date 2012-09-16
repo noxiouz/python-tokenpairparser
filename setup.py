09cabafedb500ed45bc568787a7c794190c28313
noxiouz <noxiouz@yandex.ru>
2012-09-16 22:55:33 +0400

    1

--- setup.py
+++ setup.py
@@ -6,14 +6,8 @@ except ImportError:
 setup(
     name = "TokenParser",
     version = "0.1",
-    #description = "Cocaine Python Framework",
-    #long_description = "A simple framework to ease the development of Cocaine apps",
-    #url = "https://github.com/cocaine/cocaine-framework-python",
     author = "Tyurin Anton",
     author_email = "noxiouz@yandex.ru",
-    #license = "BSD 2-Clause",
-    #platforms = ["Linux", "BSD", "MacOS"],
-    #packages = ["cocaine", "cocaine.decorators"],
     ext_modules = [Extension("_tokenparser",
                              ["src/tokenparser.cpp","src/module.cpp"],
                              include_dirs = ["include"])]
