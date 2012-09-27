cc77d85d15e2025d4c21dd0151083c9741be40c4
noxiouz <noxiouz@yandex.ru>
2012-09-28 00:55:07 +0400

    add eb build

---
+++ Tokenparser.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+from _tokenparser import *
+
+
--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ except ImportError:
     from distutils.core import setup, Extension
 
 setup(
-    name = "TokenPairParser",
+    name = "tokenparser",
     version = "0.1",
     author = "noxiouz",
+    license="GPL2",
+    description="Fast string parser",
+    long_description="Fast string parsing baesd on boost::sprit",
+    url="https://github.com/noxiouz/python-Tokenpair-parser-on-C-API",
     author_email = "noxiouz@yandex.ru",
+    py_module=["Tokenparser.py"],
     ext_modules = [Extension("_tokenparser",
                              ["src/tokenparser.cpp","src/module.cpp"],
                              include_dirs = ["include"])]
