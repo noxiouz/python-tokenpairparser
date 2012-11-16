b9c07bcfcde11ee095805db5a8490497c48b4638
noxiouz <noxiouz@yandex.ru>
2012-11-16 09:54:23 +0400

    Fix README.md and rename some methods. Add test.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author = "noxiouz",
     license="GPL2",
     description="Fast string parser",
-    long_description="Fast string parsing baesd on boost::sprit",
+    long_description="Fast string parsing based on Boost.Spirit",
     url="https://github.com/noxiouz/python-Tokenpair-parser-on-C-API",
     author_email = "noxiouz@yandex.ru",
     packages=["Tokenparser"],
--- src/module.cpp
+++ src/module.cpp
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 2012 Anton Tyurin <noxiouz@yandex.ru>
  *
- * This file is part of TokenPairParser.
+ * This file is part of Tokenparser.
  *
- * TokenPairParser is free software; you can redistribute it and/or modify
+ * Tokentarser is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * TokenPairParser is distributed in the hope that it will be useful,
+ * Tokenparser is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
