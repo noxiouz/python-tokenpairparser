55b79324554ea778a1abaf6ffd3c934274c13a18
noxiouz <noxiouz@yandex.ru>
2013-05-24 14:50:51 +0400

    First work on adaptive parser

--- Tokenparser/adaptive_parser.py
+++ Tokenparser/adaptive_parser.py
@@ -1,24 +1,64 @@
 #! /usr/bin/env python
 
 import Tokenparser
+import yaml
+from pprint import pprint
+import sys
 
+try:
+    INPUT_FILE = sys.argv[1]
+    OUTPUT_FILE = sys.argv[2]
+    CONFIG_FILE = sys.argv[3]
+except Exception as err:
+    print "Usage: prog_name my_log.log output.py config"
+    exit(1)
 
-def create_obejct(meth_and_args):
-    p = Tokenparser.Tokenparser()
-    for meth, args in meth_and_args.iteritems():
-        getattr(p,meth)(*args)
-    return p
+with open(INPUT_FILE) as f:
+    s = f.readline()
 
-def create_parser_from_YAML(path="test.yaml"):
-    import yaml
-    return create_obejct(yaml.load(open(path))['PARSER OBJECT'])
+cfg = yaml.load(open(CONFIG_FILE))
 
-def create_parser_from_JSON(path="test.json"):
-    import json
-    return create_obejct(json.load(open(path))['PARSER OBJECT'])
+class Generator(object):
+
+    def __init__(self, example, config):
+        self._cfg = config
+        self.example = example
+        self._source = list()
+        self._source.append("import Tokenparser")
+        self._source.append("from pprint import pprint")
+        self._source.append("p = Tokenparser.Tokenparser()")
+        self._source.append("example = \"\"\"%s\"\"\"" % example)
+
+    def get_borders(self, target):
+        print "Find %s" % target
+        begin = self.example.find(target)
+        end = begin + len(target)
+        return begin, end
+
+    def generate(self):
+        for k, v in (next(item.iteritems()) for item in self._cfg):
+            b, e = self.get_borders(v)
+            self.generate_command(b, e, k)
+            self.example = self.example[e:]
+        self._source.append("p.parse(example)")
+        self._source.append("pprint(p.matches())")
+        return '\n'.join(self._source)
+
+    def generate_command(self, begin, end, field):
+        if begin == 0:
+            code = "p.upTo('%s','%s')" % (field, self.example[end])
+            self._source.append(code)
+        else:
+            for i in xrange(begin):
+                self._source.append("p.skip('%s')" % self.example[i])
+            code = "p.upTo('%s', '%s')" %(field, self.example[end])
+            self._source.append(code)
+    
 
 if __name__ == "__main__":
-    f = create_parser_from_YAML()
-    print f.parse("DDDACEEED")
-    f = create_parser_from_JSON()
-    print f.parse("DDDACEEED")
+    f = open(OUTPUT_FILE, "w")
+    G = Generator(s, cfg)
+    source = G.generate()
+    print source
+    f.write(source)
+    f.close()
